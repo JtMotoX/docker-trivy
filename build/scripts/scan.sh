850631f8a5083673a52f91da8d2e91bb37dc71ff
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-09-03 12:10:43 -0700

    add ScanTime to scan results

--- build/scripts/scan.sh
+++ build/scripts/scan.sh
@@ -4,8 +4,11 @@ SCANLOGS_DIR="/var/log/scan-logs"
 
 echo "Scanning $1 . . ."
 LOGFILE="${SCANLOGS_DIR}/$1.json"
-LOGDIR="$(dirname ${LOGFILE})"
-if [ ! -d "${LOGDIR}" ]; then
-	mkdir -p "${LOGDIR}"
-fi
-trivy image $1 --security-checks vuln -f json -o "${LOGFILE}"
+TMPFILE="/tmp/$1.json"
+mkdir -p "$(dirname "${LOGFILE}")"
+mkdir -p "$(dirname "${TMPFILE}")"
+chmod 755 "${SCANLOGS_DIR}"
+find "${SCANLOGS_DIR}" -type d -exec sh -c "chmod 755 {}" \;
+find "${SCANLOGS_DIR}" -type f -exec sh -c "chmod 644 {}" \;
+trivy image $1 --security-checks vuln -f json -o "${TMPFILE}"
+jq -c --arg date "$(date +"%Y-%m-%dT%H:%M:%S%z")" '{"ScanTime": $date} + .' "${TMPFILE}" >"${LOGFILE}"
