a4effa0b5e123a9b7c2d9d4114e110ea21ab75f0
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-09-03 20:14:26 -0700

    save image results as combined and individual vulnerabilities

--- build/scripts/scan.sh
+++ build/scripts/scan.sh
@@ -1,14 +1,39 @@
 #!/bin/sh
 
-SCANLOGS_DIR="/var/log/scan-logs"
+IMAGE="$1"
 
-echo "Scanning $1 . . ."
-LOGFILE="${SCANLOGS_DIR}/$1.json"
-TMPFILE="/tmp/$1.json"
-mkdir -p "$(dirname "${LOGFILE}")"
+echo "Scanning ${IMAGE} . . ."
+
+# DEFINE VARIABLES
+SCANLOGS_DIR="/var/log/scan-logs"
+COMBINED_LOGFILE="${SCANLOGS_DIR}/combined/${IMAGE}.json"
+SPLIT_LOGDIR="${SCANLOGS_DIR}/individual/${IMAGE}"
+TMPFILE="/tmp/${IMAGE}.json"
+
+# SETUP DIRECTORIES
+mkdir -p "$(dirname "${COMBINED_LOGFILE}")"
+rm -rf "${SPLIT_LOGDIR}"
+mkdir -p "${SPLIT_LOGDIR}"
 mkdir -p "$(dirname "${TMPFILE}")"
 chmod 755 "${SCANLOGS_DIR}"
 find "${SCANLOGS_DIR}" -type d -exec sh -c "chmod 755 {}" \;
 find "${SCANLOGS_DIR}" -type f -exec sh -c "chmod 644 {}" \;
-trivy image $1 --security-checks vuln -f json -o "${TMPFILE}"
-jq -c --arg date "$(date +"%Y-%m-%dT%H:%M:%S%z")" '{"ScanTime": $date} + .' "${TMPFILE}" >"${LOGFILE}"
+
+# SCAN IMAGE
+trivy image ${IMAGE} --security-checks vuln -f json -o "${TMPFILE}"
+
+# CAPTURE THE FULL RESULTS
+jq -c -r --arg date "$(date +"%Y-%m-%dT%H:%M:%S%z")" '{"ScanTime": $date} + . | .ScanTime = $date' "${TMPFILE}" >"${COMBINED_LOGFILE}"
+
+# PARSE RESULTS INTO INDIVIDUAL ARRAYS
+VULNERABILITIES="$(jq -c -r '[(. | del(.Results)) + (.Results[] | del(.Vulnerabilities)) + .Results[].Vulnerabilities[]]' "${COMBINED_LOGFILE}")"
+
+# DUMP ARRAYS TO INDIVIDUAL FILES
+i=0
+for row in $(echo "${VULNERABILITIES}" | jq -r '.[] | @base64'); do
+	i=$((i+1))
+    _jq() {
+		echo ${row} | base64 -d | jq -c -r ${1}
+    }
+	echo $(_jq '.') >"${SPLIT_LOGDIR}/${i}.json"
+done
